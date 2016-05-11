e3f51e887475ef877a13c1f4c9927cc528b01556
Ignacio Rivas <rivasign@gmail.com>
2016-05-11 18:55:48 +0100

    Proxy all calls to /api to the backend

--- server.js
+++ server.js
@@ -5,7 +5,17 @@ var config = require('./webpack.config');
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
+  historyApiFallback: true,
+  proxy: {
+    '/api/*': {
+      target: 'http://gymia-shorty.herokuapp.com',
+      secure: false,
+      changeOrigin: true,
+      rewrite: function(req) {
+        req.url = req.url.replace(/^\/api/, '');
+      }
+    }
+  }
 }).listen(3000, 'localhost', function (err, result) {
   if (err) {
     return console.log(err);
