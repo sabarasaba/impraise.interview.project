fc164f6f5aced3359d2fcee0060b78b001395fdd
Ignacio Rivas <rivasign@gmail.com>
2016-05-11 13:29:50 +0100

    Add styling and markup for header component

--- src/js/App.js
+++ src/js/App.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div className="app-container">
+        <header className="header">
+          <div className="header__section">
+            <h1 className="header__section__title">Shooooort</h1>
+          </div>
+          <div className="header__section header__section--baseline">
+            <h3 className="header__section__subtitle">The link shortener with a long name</h3>
+          </div>
+        </header>
+
+      </div>
     );
   }
 }
