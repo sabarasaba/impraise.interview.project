d00091b5033625deb8f39a80bc60d5f90fb810cb
Ignacio Rivas <rivasign@gmail.com>
2016-05-11 16:11:06 +0100

    Only enable cta if input contains a url

--- src/js/components/UrlForm.js
+++ src/js/components/UrlForm.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 
+import { isUrl } from '../helpers/utils';
+
 class UrlForm extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isButtonEnabled: false
+    };
+  }
+
+  onInputChange(e) {
+    // this.setState({
+    //   isButtonEnabled: isUrl(e.target.value)
+    // });
+
+    this.setState({
+      isButtonEnabled: e.target.value.length > 0
+    });
+  }
 
   render () {
     return (
       <div className="form form--inline">
-        <input className="form__input" type="text" placeholder="Paste the link you want to shorten here"/>
-        <button className="form__button spaced" disabled>Shorten this link</button>
+        <input className="form__input" type="text" onChange={this.onInputChange.bind(this)} placeholder="Paste the link you want to shorten here"/>
+        <button className="form__button spaced" disabled={!this.state.isButtonEnabled}>Shorten this link</button>
       </div>
     );
   }
